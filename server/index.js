a34abe65d2400e8a8dae69043165daf9aebd4572
huoqishi <wangchaohua110@foxmail.com>
2017-12-13 16:46:07 +0800

    servessr

---
+++ server/index.js
@@ -0,0 +1,9 @@
+// const path = require('path')
+const express = require('express')
+const fileInfo = require('./build/fileinfo.js')
+const app = express()
+app.use(express.static(fileInfo.path))
+app.listen(8082, err => {
+  if (err) return console.log('启动出错', err)
+  console.log('ok,请访问 http://huoqishi.net')
+})
